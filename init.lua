d72a76ad8ddeecd52603c6ad522268a5d778533f
James Bull <jpjbull@bcsonline.com.au>
2024-03-02 23:44:48 +1100

    Add config for neovide UI
    When I want to use neovim outside the terminal I like using neovide.

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+-- setup neovide
+if vim.g.neovide then
+  vim.g.neovide_scale_factor = 0.5;
+  local alpha = function()
+    return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+  end
+  vim.g.neovide_transparency = 0.9
+  vim.g.transparency = 1
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+end
+
 return {
   -- Configure AstroNvim updates
   updater = {
